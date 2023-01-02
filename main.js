9acfe2cf9fa1b447b2130217be1fee52360fe357
CaseCeiling3373 <100453141+CaseCeiling3373@users.noreply.github.com>
2023-01-01 19:44:43 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,58 @@
+var webContainer = document.getElementById('webContainer');
+
+for(var i = 0; i < projects.length; i++){
+    const item = document.createElement('div');
+    const title = document.createElement('h2');
+    const inform = document.createElement('h4');
+    const link = document.createElement('h5');
+    const thumbnail = document.createElement('div');
+    const img = document.createElement('img');
+
+    title.innerHTML = projects[i].title;
+    link.innerHTML = "View Project";
+        const l = projects[i].link;
+    link.addEventListener('click', function(){
+        location.href = l;
+        });
+    inform.innerHTML = projects[i].inform;
+    img.src = './thumbnails/' + projects[i].img;
+
+    item.classList = "item";
+    title.classList = "title";
+    thumbnail.classList = "thumbnail";
+    inform.classList = "inform";
+
+    item.appendChild(title);
+    item.appendChild(inform);
+    item.appendChild(link);
+    item.appendChild(thumbnail);
+    thumbnail.appendChild(img);
+
+    webContainer.appendChild(item);
+}
+
+
+var banners = document.getElementsByClassName("randomStuff");
+var currIdx = 0;
+
+function nextBanner(){
+    currIdx = (currIdx + 1)%banners.length;
+    for(var i = 0; i<banners.length; i++){
+        if(i == currIdx){
+            banners[i].style.zIndex = 5;
+        }else{
+            banners[i].style.zIndex = 3;  
+        }
+    }
+}
+
+function previousBanner(){
+    currIdx = (currIdx - 1 + banners.length)%banners.length;
+    for(var i = 0; i<banners.length; i++){
+        if(i == currIdx){
+            banners[i].style.zIndex = 5;
+        }else{
+            banners[i].style.zIndex = 3;  
+        }
+    }
+}
---
+++ projects.js
@@ -0,0 +1,32 @@
+var projects = [
+    { //project
+        title : "Wordle",
+        img : "wordlepicture.png",
+        link : "https://caseceiling3373.github.io/Wordle-remake/",
+        inform : "Guess the 5 letter word and get your result. Try to find the correct answer in a streak."
+    },
+    { //project
+        title : "CPS Test",
+        img : "cpstestpic.png",
+        link : "https://caseceiling3373.github.io/CPS-TEST/",
+        inform : "Click as fast as you can to find your CPS(clicks per second)."
+    },
+    { //project
+        title : "Clicker Game",
+        img : "clickergamepic.png",
+        link : "https://caseceiling3373.github.io/Clicker/ ",
+        inform : "Click many times until you reach the first milestone on the right. You must buy every upgrade in order."
+    },
+    { //project
+        title : "Arrow Game",
+        img : "arrowgamepic.png",
+        link : "https://caseceiling3373.github.io/Arrow-Game/",
+        inform : "Fun game for Friday Night Funkin enjoyers! Type the arrow on the screen as fast as you can! If you mess up one, it ruins your entire row!"
+    },
+    { //project
+        title : "Old-Timey-Clock",
+        img : "clockpic.png",
+        link : "https://caseceiling3373.github.io/Old-Timey-Clock/",
+        inform : "Just a clock."
+    }
+] 
